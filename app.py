d05cdc1d18a5669276866661f782dadff131cd1e
kaly <kalimamadou@gmail.com>
2020-12-29 05:25:30 -0800

    Implement two free html template

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, url_for
 import os
 
 app = Flask(__name__)
@@ -8,9 +8,15 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+#for older browser
 @app.route('/favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static/img'),
-                               'tech_electronics_icon.ico', mimetype='image/vnd.microsoft.icon')
+    return send_from_directory(os.path.join(app.root_path, 'static/img'),'tech_electronics_icon.ico', mimetype='image/vnd.microsoft.icon')
+
+
 if __name__ == "__main__":
     app.run()
