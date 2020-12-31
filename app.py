de2e2bda083f5c6c6d294edb4da78ab1996eda9d
kaly <kalimamadou@gmail.com>
2020-12-31 00:40:16 -0800

    adjust the templates file and receive form information

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_from_directory, url_for
+from flask import Flask, render_template, send_from_directory, url_for, request
 import os
 
 app = Flask(__name__)
@@ -11,6 +11,15 @@ def index():
 @app.route('/<string:page_name>')
 def html_page(page_name):
     return render_template(page_name)
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict();
+        print(data)
+        return 'Form submitted'
+    else:
+        return 'something went wrong, try again!'
 
 #for older browser
 @app.route('/favicon.ico')
