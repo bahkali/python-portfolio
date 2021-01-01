7ab1368868ddd579e10b54155afc2f5df4388ff3
kaly <kalimamadou@gmail.com>
2021-01-01 02:03:05 -0800

    write file using csv module

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, redirect, render_template, send_from_directory, url_for, request
 import os
+import csv
 
 app = Flask(__name__)
 
@@ -12,6 +13,15 @@ def index():
 def html_page(page_name):
     return render_template(page_name)
 
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict();
+        write_to_csv(data)
+        return redirect('/thankyou.html')
+    else:
+        return 'something went wrong, try again!'
+
 def write_to_file(data):
     with open('database.txt', mode='a') as database:
         email = data["email"]
@@ -19,16 +29,15 @@ def write_to_file(data):
         message = data["message"]
         file = database.write(f'\n{email}, {subject}, {message}')
 
-@app.route('/submit_form', methods=['POST', 'GET'])
-def submit_form():
-    if request.method == 'POST':
-        data = request.form.to_dict();
-        write_to_file(data)
-        return redirect('/thankyou.html')
-    else:
-        return 'something went wrong, try again!'
+def write_to_csv(data):
+    with open('database.csv', mode='a', newline='') as database2:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        csv_writer = csv.writer(database2, delimiter=',',  quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email,subject,message])
 
-#for older browser
+# for older browser
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static/img'),'tech_electronics_icon.ico', mimetype='image/vnd.microsoft.icon')
